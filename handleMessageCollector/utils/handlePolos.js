384b3134a406bb50a5fe3052a8f244f97764825f
ZAPHODh <102484511+ZAPHODh@users.noreply.github.com>
2022-09-21 22:22:17 -0300

    finished assistent 1.0

--- handleMessageCollector/utils/handlePolos.js
+++ handleMessageCollector/utils/handlePolos.js
@@ -6,7 +6,7 @@ const handlePolos=async (client,message,content,collector)=>{
 	const poloSelectedByID = Polos.filter(polo => polo.id == content);
 	if(poloSelectedByID.length >0){
 	 poloSelectedByID.map(async(polo)=>{
-		await client.sendText(message.from,`Polo selecionado: ${polo.polo}. Você pode mandar um email para ${polo.email}. Se preferir, você pode mandar mensagem ou ligar para o contato https://wa.me/55${polo.tel}${polo.tel2?` ou para https://wa.me/55${polo.tel2}`:""}`)
+		await client.sendText(message.from,`Polo selecionado: ${polo.polo}. Você pode mandar um email para ${polo.email}. Se preferir, você pode ${polo.WPP?'mandar  mensagem ou ligar para o contato https://wa.me/55'+polo.tel: 'ligar para o contato '+ polo.tel} para que o setor responsável possa analisar, orientar e/ou atender sua solicitação.`)
 		collector.stop()
 		handleIgnore('remove',message)
 		return
